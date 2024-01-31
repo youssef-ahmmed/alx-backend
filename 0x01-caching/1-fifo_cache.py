23b0421e9b142afbb96887debf5de66ac76a98ad
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-31 08:29:51 +0200

    Task1[Add]: Implement FIFO caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""FIFO caching"""
+from typing import Any
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO caching system"""
+
+    def __init__(self) -> None:
+        """ Initialize of FIFO and call the base"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item) -> None:
+        """ Store the data in FIFO policy"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discard_element = self.queue.pop(0)
+            self.cache_data.pop(discard_element)
+            print(f'DISCARD: {discard_element}')
+
+        self.queue.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key) -> Any:
+        """ Get data from a FIFO cache system"""
+        if key:
+            return self.cache_data.get(key, None)
