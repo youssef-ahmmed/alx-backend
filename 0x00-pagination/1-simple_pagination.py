429f4b1a41580cf677ff6402e57bad4d1172746a
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-02 08:42:26 +0200

    Task1[Add]: Implement simple pagination for csv file

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Implement Simple pagination"""
+import csv
+import math
+from typing import Tuple, List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns the start and end indices of a page"""
+    start_idx = (page - 1) * page_size
+    end_idx = page * page_size
+
+    return start_idx, end_idx
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """ Init of class server
+        """
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """ Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Get the pages of the dataset
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_idx, end_idx = index_range(page, page_size)
+
+        return self.__dataset[start_idx:end_idx]
