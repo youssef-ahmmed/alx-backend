9e385fd9d5c21a0bf12141b385c36c538569004e
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-02 08:49:34 +0200

    Task0[Edit]: Implement simple helper function {type annotations}

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Returns the start and end indices of a page"""
-    start_idx = (page - 1) * page_size
-    end_idx = page * page_size
+    start_idx: int = (page - 1) * page_size
+    end_idx: int = page * page_size
 
     return start_idx, end_idx
