07047fb60c94b7395c43e77d00ef6efaefc361b1
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-06 14:09:45 +0200

    Task1[Add]: Implement Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Basic Babel setup"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Babel basic configurations"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Default route"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
