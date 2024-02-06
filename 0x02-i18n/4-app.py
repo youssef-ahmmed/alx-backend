93bf5d7bb2bbd563266d99a8a474d1e8820a6832
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-06 16:16:35 +0200

    Task4[Add]: Force locale with URL parameter

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Force locale with URL parameter"""
+
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Babel basic configurations"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+def get_locale() -> str:
+    """Get locale"""
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app)
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Default route"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
