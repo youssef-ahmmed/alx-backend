6ede9b9fc448793bb883ec34f08e62508dc32372
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-06 15:54:51 +0200

    Task3[Add]: Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Parametrize templates"""
+
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Babel basic configurations"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Default route"""
+    # home_title = gettext("Welcome to Holberton")
+    # home_header = gettext("Hello world!")
+
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
