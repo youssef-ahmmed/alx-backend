d2f1ba563bbed061a3379281048bea32a8c6563e
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-07 08:30:08 +0200

    Task5[Edit]: Mock logging in {check for g.user}

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -8,6 +8,8 @@ from flask_babel import Babel
 
 class Config:
     """Babel basic configurations"""
+
+    DEBUG = True
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -25,16 +27,6 @@ users = {
 }
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """Get locale"""
-    locale = request.args.get('locale', '').strip()
-    if locale and locale in Config.LANGUAGES:
-        return locale
-
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 def get_user() -> Union[Dict, None]:
     """Returns a user dictionary"""
     login_as = request.args.get('login_as')
@@ -47,10 +39,20 @@ def before_request() -> None:
     g.user = get_user()
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale"""
+    locale = request.args.get('locale', '')
+    if locale in Config.LANGUAGES:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """Default route"""
-    username = g.user.get('name')
+    username = g.user.get('name') if g.user else None
     return render_template('5-index.html', username=username)
 
 
