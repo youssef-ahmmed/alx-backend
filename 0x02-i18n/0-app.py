237afef31bd78ea8809553b1d48302929a518342
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-07 08:23:35 +0200

    Task0[Edit]: Basic Flask app {strict slashes}

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,7 +4,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/', strict_slashes=False)
@@ -14,4 +13,4 @@ def index() -> str:
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
