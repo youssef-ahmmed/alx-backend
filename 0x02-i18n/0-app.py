51e710f9ce0b7cbfa9f204f68900e76b5b3c2df5
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-07 08:23:19 +0200

    Task0[Edit]: Basic Flask app {strict slashes}

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
     """Default route"""
     return render_template('0-index.html')
